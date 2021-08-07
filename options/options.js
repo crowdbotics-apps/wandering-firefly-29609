bb3c8714937583d9784224e9d08f5590482595f7
Crowdbotics <team@crowdbotics.com>
2021-08-07 14:53:59 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "wandering_firefly_29609Identifier",
+  url: "https://wandering_firefly_29609Identifier.botics.co",
+  api: "https://wandering_firefly_29609Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
